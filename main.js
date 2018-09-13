3f45ae4432a2793be285d0055cacf6b061ccac30
zzyo96 <1977795940@qq.com>
2018-09-13 14:29:23 +0800

    第一次提交button按钮没处理好

---
+++ main.js
@@ -0,0 +1,64 @@
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({transform:'translateX(-920px)'})
+bindEvents()
+
+let timer = setInterval(function(){
+	goToSlide(current+1)
+},3000)
+$('.container').on('mouseenter',function(){
+	window.clearInterval(timer)
+}).on('mouseleave',function(){
+	timer = setInterval(function(){
+		goToSlide(current+1)
+	},3000)
+})
+
+function bindEvents(){
+	$('#buttonWrapper').on('click','button',function(e){
+		let $button = $(e.currentTarget)
+		let index = $button.index()
+		goToSlide(index)
+	})
+}
+
+//重要
+function goToSlide(index){
+	if(index > $buttons.length-1){
+		index = 0
+	}else if(index < 0){
+		index = $buttons.length - 1
+	}
+	if(current === $buttons.length - 1 && index === 0){
+		//最后一张到第一张
+		$slides.css({transform:`translateX(${-($buttons.length + 1) * 920}px)`})
+			.one('transitionend',function(){
+        $slides.hide()
+        $slides.offset()
+				$slides.css({transform:`translateX(${-(index+1) * 920}px)`}).show()
+			})
+	}else if(current === 0 && index === $buttons.length - 1){
+		//第一张到最后一张
+		$slides.css({transform:`translateX(0px)`})
+			.one('transitionend',function(){
+				$slides.hide().offset()
+				$slides.css({transform:`translateX(${-(index+1)*920}px)`}).show()
+			})
+
+	}else{
+		$slides.css({transform:`translateX(${-(index+1)*920}px)`})
+	}
+	current = index
+}
+
+function makeFakeSlides(){
+	let $firstCopy = $images.eq(0).clone(true)
+	let $lastCopy = $images.eq($images.length-1).clone(true)
+
+	$slides.append($firstCopy)
+	$slides.prepend($lastCopy)
+}
